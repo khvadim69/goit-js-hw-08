17a3f191c45ebf7ec7214045aa2d9794b6029ff7
khvadim69 <101789017+khvadim69@users.noreply.github.com>
2023-03-08 13:41:50 +0200

    1234

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,49 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+console.log(galleryContainer);
+galleryContainer.addEventListener("click", handleGaleryContainerClick);
+
+function createGalleryCardsMarkup(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `
+    
+        <a class="gallery__item" href="${original}";
+        >
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            title = ${description};
+          />
+        </a>
+    
+    `;
+    })
+    .join("");
+}
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryContainer.addEventListener("click", handleGaleryContainerClick);
+function handleGaleryContainerClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(event.target);
+  let href = event.target.closest("a").getAttribute("href");
+  return href;
+}
+let gallery = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  scrollZoom: false,
+});
+gallery.on("show.simplelightbox", function () {});
+gallery.on("error.simplelightbox", function (event) {
+  console.log(event);
+});
