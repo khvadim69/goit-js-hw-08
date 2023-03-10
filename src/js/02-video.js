c158e7348f2976dbef890e397a6f0cd3dba5829b
khvadim69 <101789017+khvadim69@users.noreply.github.com>
2023-03-10 20:05:45 +0200

    Друге  завдання готове

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', function (time) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+});
+const playerSaveTime = localStorage.getItem('videoplayer-current-time');
+// console.log(playerSaveTime);
+const playerTimeStop = JSON.parse(playerSaveTime);
+// console.log(playerTimeStop.seconds);
+player.setCurrentTime(playerTimeStop.seconds || 0);
