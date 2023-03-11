53ba7e842aa8c43d27ce5e1fc14bbc58519a1ec1
khvadim69 <101789017+khvadim69@users.noreply.github.com>
2023-03-11 17:18:23 +0200

    Добавив тротел

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const form = document.querySelector(".feedback-form");
 
+form.addEventListener("input", handleInput);
+function handleInput(event) {
+  event.preventDefault();
+  localStorage.setItem('feedback-form-state', JSON.stringify(email,message));
+  const {
+    elements: { email, message }
+  } = event.currentTarget;
+
+  if (email.value === "" || message.value === "") {
+  localStorage.getItem('feedback-form-state');
+  }
+
+  console.log(`email: ${email.value}, message: ${message.value}`);
+  event.currentTarget.reset();
+};
+
+  
+
+
+
+// const form = document.querySelector(".form");
+
+// form.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { login, password }
+//   } = event.currentTarget;
+
+//   if (login.value === "" || password.value === "") {
+//     return console.log("Please fill in all the fields!");
+//   }
+
+//   console.log(`Login: ${login.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
