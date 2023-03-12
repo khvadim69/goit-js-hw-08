dfbe56b067e91cd494a01fcca9b262e8f5224d59
khvadim69 <101789017+khvadim69@users.noreply.github.com>
2023-03-12 21:07:13 +0200

    3 задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,27 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle'
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-player.on('timeupdate', throttle (function (time) {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
-  
-},1000));
+player.on(
+  'timeupdate',
+  throttle(function (time) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+  }, 1000)
+);
 const playerSaveTime = localStorage.getItem('videoplayer-current-time');
 // console.log(playerSaveTime);
 const playerTimeStop = JSON.parse(playerSaveTime);
 // console.log(playerTimeStop.seconds);
-player.setCurrentTime(playerTimeStop.seconds).then(function(seconds) {
-}).catch(function(error) {
-  switch (error.name) {
+player
+  .setCurrentTime(playerTimeStop.seconds)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
       case 'RangeError':
-                  break;
+        break;
 
       default:
-                  break;
-  }
-});
+        break;
+    }
+  });
 // player.setCurrentTime(playerTimeStop.seconds || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,68 @@
-const form = document.querySelector(".feedback-form");
-import throttle from 'lodash.throttle'
-form.addEventListener("input", handleInput);
-throttle(function handleInput(event) {
+
+
+const form = document.querySelector('.feedback-form');
+import throttle from 'lodash.throttle';
+const STORAGE_KEY= 'feedback-form-state'
+form.addEventListener(
+  'input',
+  throttle(event => {
+    event.preventDefault();
+    // const { email, message } = event.target;
+    const { email, message } = event.currentTarget;
+    const formData = {};
+    formData.email = email.value.trim();
+    formData.message = message.value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
   event.preventDefault();
-  localStorage.setItem('feedback-form-state', JSON.stringify(email,message));
-  const {
-    elements: { email, message }
-  } = event.currentTarget;
-
-  if (email.value === "" || message.value === "") {
-  localStorage.getItem('feedback-form-state');
-  }
-
-  console.log(`email: ${email.value}, message: ${message.value}`);
+  // event.target.reset();
   event.currentTarget.reset();
-},500);
+  localStorage.removeItem(STORAGE_KEY);
+});
+saveForm();
+function saveForm() {
+  const complectedForm = JSON.parse(
+    localStorage.getItem(STORAGE_KEY)
+  );
+  if (complectedForm) {
+    form.email.value = complectedForm.email;
+    form.message.value = complectedForm.message;
+  }
+}
 
-  
-// output.textContent = event.currentTarget.value;
 
 
 
-// const form = document.querySelector(".form");
+// const form = document.querySelector('.feedback-form');
+// import throttle from 'lodash.throttle';
+// const STORAGE_KEY= 'feedback-form-state'
+// form.addEventListener(
+//   'input',
+//   throttle(event => {
+//     event.preventDefault();
+//     const { email, message } = event.currentTarget;
+//     const formData = {};
+//     formData.email = email.value.trim();
+//     formData.message = message.value.trim();
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+//   }, 500)
+// );
 
-// form.addEventListener("submit", handleSubmit);
-
-// function handleSubmit(event) {
+// form.addEventListener('submit', event => {
 //   event.preventDefault();
-//   const {
-//     elements: { login, password }
-//   } = event.currentTarget;
-
-//   if (login.value === "" || password.value === "") {
-//     return console.log("Please fill in all the fields!");
-//   }
-
-//   console.log(`Login: ${login.value}, Password: ${password.value}`);
 //   event.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+// });
+// saveForm();
+// function saveForm() {
+//   const complectedForm = JSON.parse(
+//     localStorage.getItem(STORAGE_KEY)
+//   );
+//   if (complectedForm) {
+//     form.email.value = complectedForm.email;
+//     form.message.value = complectedForm.message;
+//   }
+// }
