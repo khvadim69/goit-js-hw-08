c5c85c5a438e72fbf19701082aa2c024f6616fa1
khvadim69 <101789017+khvadim69@users.noreply.github.com>
2023-03-13 21:52:53 +0200

    ще трохи

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,18 @@
 const form = document.querySelector('.feedback-form');
 import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
-let formData = {}
-form.addEventListener('input', throttle(event =>{
-    formData[event.target.name] = event.target.value; onInputFormValue();
-}, 500));
+let formData = {};
+form.addEventListener(
+  'input',
+  throttle(event => {
+    formData[event.target.name] = event.target.value;
+    onInputFormValue();
+  }, 500)
+);
 function onInputFormValue() {
-    const inputValue = JSON.stringify(formData);
-    localStorage.setItem(CURRENT_VALUE, inputValue);
-};
+  const inputValue = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, inputValue);
+}
 saveForm();
 function saveForm() {
   const complectedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
@@ -18,9 +22,9 @@ function saveForm() {
   }
 }
 form.addEventListener('submit', event => {
-    event.preventDefault();
-    console.log(formData)
-    event.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-    Object.keys(formData).forEach(el => delete formData[el]);
+  event.preventDefault();
+  console.log(formData);
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  Object.keys(formData).forEach(el => delete formData[el]);
 });
